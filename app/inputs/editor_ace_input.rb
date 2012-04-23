7e593fabaa3fce8ada7d13b28fe1cfe1a10aa120
Peter Horn <peter.horn@provideal.net>
2012-04-23 18:21:04 +0200

    renaming to avoid clashes

--- app/inputs/editor_ace_input.rb
+++ app/inputs/editor_ace_input.rb
@@ -1,4 +1,4 @@
-class AceEditorInput < SimpleForm::Inputs::Base
+class EditorAceInput < SimpleForm::Inputs::Base
   def input
     id = "#{object.class.name.underscore}_#{attribute_name}"
     "<div class=\"ace-editor-wrapper\"><pre id=\"ace-editor-#{id.gsub('_',"-")}\" data-ace-editor-target=\"#{id}\"></pre>#{@builder.text_area(attribute_name, input_html_options)}</div>".html_safe

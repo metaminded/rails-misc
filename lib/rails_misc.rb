5d318b2bb0366081e2f22a2e08695e42c47c18bf
Peter Horn <peter.horn@provideal.net>
2012-06-11 10:35:05 +0200

    fill_form

--- lib/rails_misc.rb
+++ lib/rails_misc.rb
@@ -1,5 +1,6 @@
 require "rails_misc/engine"
 require "rails_misc/act_as_matchcoded"
+require "rails_misc/fill_form"
 
 module RailsMisc
 end
---
+++ lib/rails_misc/fill_form.rb
@@ -0,0 +1,39 @@
+class ActionDispatch::IntegrationTest
+
+  MONTH_NAMES = %w{January February March April May June July August September October November December}
+
+  def fill_form(form_name, params, submit)
+    params.each do |param_name, val|
+      id = "#{form_name}_#{param_name}"
+      case val
+      when String then fill_in id, with: val
+      when Symbol then select val.to_s, from: id
+      when true then check(id)
+      when false then uncheck(id)
+      when Time then
+        select val.year.to_s, from: "#{id}_1i"
+        select MONTH_NAMES[val.month], from: "#{id}_2i"
+        select val.day.to_s, from: "#{id}_3i"
+        select val.hour.to_s, from: "#{id}_4i"
+        select val.min.to_s, from: "#{id}_5i"
+      when Date then
+        select val.year.to_s, from: "#{id}_1i"
+        select MONTH_NAMES[val.month], from: "#{id}_2i"
+        select val.day.to_s, from: "#{id}_3i"
+      when File then attach_file(id, val.to_path)
+      else raise "Don't know what to do with `#{val}` on `#{id}`."
+      end
+    end
+    yield if block_given?
+    click_button submit
+  end
+
+  def assert_path_is(path)
+    assert_equal path, current_path
+  end
+
+  def assert_content(content)
+    assert page.has_content?(content)
+  end
+
+end

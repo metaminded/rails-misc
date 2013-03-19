19be5494d85f3d1baeb17ff41e546c1bbe980a8c
Peter Horn <peter.horn@provideal.net>
2013-03-19 13:06:13 +0100

    find by id

--- lib/rails_misc/act_as_matchcoded.rb
+++ lib/rails_misc/act_as_matchcoded.rb
@@ -4,7 +4,11 @@ class ActiveRecord::Base
     validates_format_of matchcode, :with => MATCHCODE_PATTERN, :allow_nil => true
     validates_uniqueness_of matchcode, :allow_nil => true
     self.define_singleton_method :[] do |m|
-      self.send("find_by_#{matchcode}", m)
+      if m.is_a?(Fixnum) || (m.is_a?(String) && /\A\d+\Z/.match(m))
+        self.find(m)
+      else
+        self.send("find_by_#{matchcode}", m)
+      end
     end
   end
 end
